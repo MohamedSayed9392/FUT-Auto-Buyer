e9afb19eb7d93738d7ffb6380a9ff118ece0d86a
Mohamed Sayed <MohamedSayed9392@gmail.com>
2022-11-04 11:18:13 +0200

    update version number

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -2,7 +2,7 @@ module.exports = {
   headers: {
     name: "FUT Auto Buyer [Edited]",
     namespace: "http://tampermonkey.net/",
-    version: "1.5.0 [Edited]",
+    version: "1.5.1 [Edited]",
     description: "FUT Auto Buyer",
     author: "CK Algos",
     match: [
